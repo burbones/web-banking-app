3425623f99de24dd09379ff92411393de5354b3d
olga.liberzon <olia.argh@gmail.com>
2024-09-10 17:10:25 +0300

    Added dashboard request

--- frontend/web-banking-frontend/src/components/Dashboard.js
+++ frontend/web-banking-frontend/src/components/Dashboard.js
@@ -1,19 +1,63 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Sidebar from "./low_level/Sidebar";
-import { Box, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, CircularProgress, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios, { HttpStatusCode } from "axios";
+import { LOGIN_URL, SERVER_DASHBOARD_URL } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../authSlice";
 
 export default function Dashboard() {
+    const [data, setData] = useState(null);
     const token = useSelector((state) => state.auth.token);
-    console.log(token);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        axios.get(SERVER_DASHBOARD_URL, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        .then((res) => {
+            setData(res.data);
+        })
+        .catch((err) => {
+            if (err.status === HttpStatusCode.Unauthorized) {
+                dispatch(setUser({token: ""}));
+                navigate(LOGIN_URL);
+            }
+        })
+
+    }, [token, navigate, dispatch]);
 
     return (
         <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
             <Sidebar />
-            <Box p="4">
-                <Heading>
-                Dashboard
-                </Heading>
+            <Box p="4"> {!data ? <CircularProgress isIndeterminate color='green.300' size='20' /> :
+                    <>
+                        <Heading mb={5}>
+                            Dashboard
+                        </Heading>
+                        <Text fontSize='xl' mb={5}>
+                            <b>Current balance:</b> {"$" + data.balance}
+                        </Text>
+                        <TransactionList transactions={data.transactions} />
+                    </>
+                }
             </Box>
         </Box>
     );
+}
+
+function TransactionList(props) {
+    return (
+        <>
+            <Text fontSize='xl'>
+                <b>Transactions:</b>
+                {props.transactions.map((transaction) => <p>{transaction.type}</p>)}
+            </Text>
+        </>
+    );
+
 }
--- frontend/web-banking-frontend/src/utils/constants.js
+++ frontend/web-banking-frontend/src/utils/constants.js
@@ -17,3 +17,4 @@ export const TRANSFERS_URL = "/transaction";
 export const SERVER_LOGIN_URL = getBaseServerURL() + "api/v1" + LOGIN_URL;
 export const SERVER_SIGN_UP_URL = getBaseServerURL() + "api/v1" + SIGNUP_URL;
 export const SERVER_CODE_CHECK_URL = SERVER_SIGN_UP_URL + "/verifyCode";
+export const SERVER_DASHBOARD_URL = getBaseServerURL() + "api/v1" + DASHBOARD_URL;
